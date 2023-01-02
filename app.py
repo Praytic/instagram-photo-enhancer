65050c8c8b6f7838bd6ae00085b88a04318f6f47
Praytic <chernogorovvm@gmail.com>
2023-01-02 01:40:04 -0500

    Use blueprints to build an app

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import core
 import os
-
-from functools import wraps
-from requests_oauthlib import OAuth2Session
-from flask import Flask, request, redirect, session, url_for
-from flask.json import jsonify
 from logging.config import dictConfig
+from flask import Flask
+from routes import routes_blueprint
 
 
 dictConfig({
@@ -24,117 +20,19 @@ dictConfig({
     }
 })
 
+app = Flask(__name__)
+app.logger.info("Starting the app...")
+app.register_blueprint(routes_blueprint)
 
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.logger.debug("Starting the app")
-    app.config.update(CLIENT_ID=os.getenv("CLIENT_ID"),
-                      CLIENT_SECRET=os.getenv("CLIENT_SECRET"),
-                      REDIRECT_URI=os.getenv("REDIRECT_URI"),
-                      AUTHORIZATION_BASE_URL='https://api.instagram.com/oauth/authorize',
-                      USER_URL='https://graph.instagram.com/me?fields=id,username',
-                      TOKEN_URL='https://api.instagram.com/login/oauth/access_token',
-                      ACCESS_TOKEN=os.getenv("ACCESS_TOKEN"),
-                      PORT = int(os.getenv("PORT", 5000)))
-    app.secret_key = os.urandom(24)
-    return app
+app.config.update(CLIENT_ID=os.getenv("CLIENT_ID"),
+                  CLIENT_SECRET=os.getenv("CLIENT_SECRET"),
+                  REDIRECT_URI=os.getenv("REDIRECT_URI"),
+                  AUTHORIZATION_BASE_URL='https://api.instagram.com/oauth/authorize',
+                  USER_URL='https://graph.instagram.com/me?fields=id,username',
+                  TOKEN_URL='https://api.instagram.com/login/oauth/access_token',
+                  ACCESS_TOKEN=os.getenv("ACCESS_TOKEN"),
+                  PORT=int(os.getenv("PORT", 5000)))
+app.secret_key = os.urandom(24)
 
-def create_gunicorn_app() -> Flask:
-    app = create_app()
-    app.run(host='0.0.0.0', port=port)
-
-def create_local_app() -> Flask:
-    app = create_app()
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
-    app.config.from_envvar(".env", True)
-    app.run(host='0.0.0.0', port=port, debug=True)
-
-
-app = create_local_app() if __name__ == "__main__" else create_gunicorn_app()
-
-
-class SubmitResponse(object):
-    def __init__(self, **kwargs):
-        self.renderingInProgress = True
-
-
-@app.route("/")
-def login():
-    """Step 1: User Authorization.
-
-    Redirect the user/resource owner to the OAuth provider 
-    using an URL with a few key OAuth parameters.
-    """
-    app.logger.debug("Login the client. %s", app.config['CLIENT_ID'])
-    instagram = OAuth2Session(
-        app.config['CLIENT_ID'], scope='user_profile', redirect_uri=app.config['REDIRECT_URI'])
-    authorization_url, state = instagram.authorization_url(
-        app.config['AUTHORIZATION_BASE_URL'])
-
-    # State is used to prevent CSRF, keep this for later.
-    session['oauth_state'] = state
-    return redirect(authorization_url)
-
-
-# Step 2: User authorization, this happens on the provider.
-
-@app.route("/callback", methods=["GET"])
-def callback():
-    """ Step 3: Retrieving an access token.
-
-    The user has been redirected back from the provider to your registered
-    callback URL. With this redirection comes an authorization code included
-    in the redirect URL. We will use that to obtain an access token.
-    """
-    app.logger.debug("Callback func. Request params: %s", request.args)
-    instagram = OAuth2Session(
-        app.config['CLIENT_ID'], state=session['oauth_state'])
-    if app.config['ACCESS_TOKEN']:
-        token = app.config['ACCESS_TOKEN']
-    else:
-        token = instagram.fetch_token(
-            app.config['TOKEN_URL'], client_secret=app.config['CLIENT_SECRET'], authorization_response=request.url)
-
-    # At this point we can fetch protected resources but lets save
-    # the token and show how this is done from a persisted token
-    # in /profile.
-    session['oauth_token'] = token
-
-    return redirect(url_for('.profile'))
-
-
-def token_required(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
-        token = None
-        app.logger.debug("Checking oauth_token...")
-        # ensure the jwt-token is passed with the headers
-        if session['oauth_token']:
-            app.logger.debug("oauth_token: %s", session['oauth_token'])
-            token = session['oauth_token']
-        if not token:  # throw error if no token provided
-            app.logger.debug("oauth_token doesn't exist, redirecting to /")
-            return redirect(url_for('.'))
-        # Return the user information attached to the token
-        return f(token, *args, **kwargs)
-    return decorator
-
-
-@app.route("/profile", methods=["GET"])
-@token_required
-def profile(token):
-    """Fetching a protected resource using an OAuth 2 token.
-    """
-    app.logger.debug("Returning user profile")
-    instagram = OAuth2Session(app.config['CLIENT_ID'], token=token)
-    return jsonify(instagram.get(app.config['USER_URL']).json())
-
-
-@app.route('/submit', methods=['POST'])
-@token_required
-def submit():
-    app.logger.debug("Submit image processing")
-    img = request.files.get('img', '')
-    selectionRect = request.files.get('selectionRect', '')
-    core.VideoProcessor(img)
-    return SubmitResponse()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=app.config['PORT'], debug=True)
---
+++ routes.py
@@ -0,0 +1,96 @@
+from functools import wraps
+
+from flask import redirect, request, session, url_for, Blueprint, current_app
+from flask.json import jsonify
+from requests_oauthlib import OAuth2Session
+
+import core
+
+routes_blueprint = Blueprint('routes_blueprint', __name__)
+
+
+class SubmitResponse(object):
+    def __init__(self, **kwargs):
+        self.renderingInProgress = True
+
+
+@routes_blueprint.route("/",  methods=["GET"])
+def login():
+    """Step 1: User Authorization.
+
+    Redirect the user/resource owner to the OAuth provider 
+    using an URL with a few key OAuth parameters.
+    """
+    current_app.logger.debug("Login the client. %s", current_app.config['CLIENT_ID'])
+    instagram = OAuth2Session(
+        current_app.config['CLIENT_ID'], scope='user_profile', redirect_uri=current_app.config['REDIRECT_URI'])
+    authorization_url, state = instagram.authorization_url(
+        current_app.config['AUTHORIZATION_BASE_URL'])
+
+    # State is used to prevent CSRF, keep this for later.
+    session['oauth_state'] = state
+    return redirect(authorization_url)
+
+
+# Step 2: User authorization, this happens on the provider.
+
+@routes_blueprint.route("/callback", methods=["GET"])
+def callback():
+    """ Step 3: Retrieving an access token.
+
+    The user has been redirected back from the provider to your registered
+    callback URL. With this redirection comes an authorization code included
+    in the redirect URL. We will use that to obtain an access token.
+    """
+    current_app.logger.debug("Callback func. Request params: %s", request.args)
+    instagram = OAuth2Session(
+        current_app.config['CLIENT_ID'], state=session['oauth_state'])
+    if current_app.config['ACCESS_TOKEN']:
+        token = current_app.config['ACCESS_TOKEN']
+    else:
+        token = instagram.fetch_token(
+            current_app.config['TOKEN_URL'], client_secret=current_app.config['CLIENT_SECRET'], authorization_response=request.url)
+
+    # At this point we can fetch protected resources but lets save
+    # the token and show how this is done from a persisted token
+    # in /profile.
+    session['oauth_token'] = token
+
+    return redirect(url_for('.profile'))
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        current_app.logger.debug("Checking oauth_token...")
+        # ensure the jwt-token is passed with the headers
+        if session['oauth_token']:
+            current_app.logger.debug("oauth_token: %s", session['oauth_token'])
+            token = session['oauth_token']
+        if not token:  # throw error if no token provided
+            current_app.logger.debug("oauth_token doesn't exist, redirecting to /")
+            return redirect(url_for('.'))
+        # Return the user information attached to the token
+        return f(token, *args, **kwargs)
+    return decorator
+
+
+@routes_blueprint.route("/profile", methods=["GET"])
+@token_required
+def profile(token):
+    """Fetching a protected resource using an OAuth 2 token.
+    """
+    current_app.logger.debug("Returning user profile")
+    instagram = OAuth2Session(current_app.config['CLIENT_ID'], token=token)
+    return jsonify(instagram.get(current_app.config['USER_URL']).json())
+
+
+@routes_blueprint.route('/submit', methods=['POST'])
+@token_required
+def submit():
+    current_app.logger.debug("Submit image processing")
+    img = request.files.get('img', '')
+    selectionRect = request.files.get('selectionRect', '')
+    core.VideoProcessor(img)
+    return SubmitResponse()
